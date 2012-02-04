8d43a2c8d88eb6bcf4e9f65bb1182d68a8ccde05
Jonathon Mah <me@JonathonMah.com>
2012-02-04 04:02:10 -0800

    Error handling and stuff
    It's now quite possible to play a game in the shell!

--- src/ttt_game.erl
+++ src/ttt_game.erl
@@ -8,8 +8,8 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/1, print_board/1, board_to_iolist/1]).
--export([pieces_added/2, board_state/1]).
+-export([start/1, make_move/3, get_board/1]).
+-export([print_board/1, board_to_iolist/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -26,8 +26,21 @@
 -type square() :: piece() | 'u'.
 -type board() :: [[square(),...],...].
 
-start_link({{_OPid,_ORef},{_XPid,_XRef}}=PlayerInfo) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, PlayerInfo, []).
+start({{_OPid,_ORef},{_XPid,_XRef}}=PlayerInfo) ->
+    gen_server:start(?MODULE, PlayerInfo, []).
+
+-spec make_move(any(), reference(), board()) -> ok | {error, any()}.
+make_move(ServerRef, ClientRef, NewBoard) ->
+    case is_valid_board(NewBoard) of
+        true ->
+            gen_server:call(ServerRef, {make_move, ClientRef, NewBoard});
+        false ->
+            {error, invalid_board}
+    end.
+
+-spec get_board(any()) -> board().
+get_board(ServerRef) ->
+    gen_server:call(ServerRef, get_board, 5000).
 
 -spec print_board(board()) -> ok.
 print_board(Board) ->
@@ -50,39 +63,49 @@ board_to_iolist(Board) ->
 -record(st, { players :: [#player{}], board=[[u,u,u],[u,u,u],[u,u,u]] :: board() }).
 
 init({{OPid,ORef},{XPid,XRef}}) ->
-    % TODO: Monitor
+    % TODO: Monitor players
     OPlayer = #player{pid=OPid, ref=ORef, piece=o},
     XPlayer = #player{pid=XPid, ref=XRef, piece=x},
-    notify_player(OPlayer, your_turn),
-    {ok, #st{players=[OPlayer, XPlayer]}}.
+    NewState = #st{players=[OPlayer, XPlayer]},
+    notify_player(OPlayer, {your_move, OPlayer#player.piece, NewState#st.board}),
+    {ok, NewState}.
+
+handle_call(get_board, _From, State) ->
+    {reply, State#st.board, State};
+
+handle_call({make_move, Ref, NewBoard}, From, State) ->
+    [ThisPlayer, NextPlayer] = State#st.players,
+
+    % Verify ref and that the move was legal
+    case validate_move(ThisPlayer, State#st.board, Ref, NewBoard) of
+        ok ->
+            NewState = State#st{players=[NextPlayer, ThisPlayer], board=NewBoard},
+            % Reply happens here!
+            gen_server:reply(From, ok),
+
+            case board_state(NewBoard) of
+                {winner, Piece} when Piece == ThisPlayer#player.piece ->
+                    % Only current player can win
+                    notify_player(ThisPlayer, {you_win, NewBoard}),
+                    notify_player(NextPlayer, {you_lose, NewBoard}),
+                    {stop, normal, NewState};
+
+                tie ->
+                    notify_player(ThisPlayer, {tie, NewBoard}),
+                    notify_player(NextPlayer, {tie, NewBoard}),
+                    {stop, normal, NewState};
+
+                ongoing ->
+                    notify_player(NextPlayer, {your_move, NextPlayer#player.piece, NewBoard}),
+                    {noreply, NewState}
+            end;
+
+        {error, _}=Error ->
+            {reply, Error, State}
+    end;
 
 handle_call(_Req, _From, State) ->
     {reply, ignored, State}.
-
-handle_cast({move, NewBoard, Ref}, State) ->
-    [#player{piece = ThisPiece}=ThisPlayer, NextPlayer] = State#st.players,
-
-    Ref = ThisPlayer#player.ref,
-    [ThisPiece] = pieces_added(State#st.board, NewBoard),
-
-    NewState = State#st{players=[NextPlayer, ThisPlayer], board=NewBoard},
-
-    case board_state(NewBoard) of
-        {winner, ThisPiece} ->
-            % Only current player can win
-            notify_player(ThisPlayer, {you_win, NewBoard}),
-            notify_player(NextPlayer, {you_lose, NewBoard}),
-            {stop, normal, NewState};
-
-        tie ->
-            notify_player(ThisPlayer, {tie, NewBoard}),
-            notify_player(NextPlayer, {tie, NewBoard}),
-            {stop, normal, NewState};
-
-        ongoing ->
-            notify_player(NextPlayer, your_turn),
-            {noreply, NewState}
-    end;
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -104,6 +127,40 @@ code_change(_OldVsn, State, _Extra) ->
 square_to_char(o) -> $O;
 square_to_char(x) -> $X;
 square_to_char(u) -> $ .
+
+-spec is_valid_board(board()) -> boolean().
+is_valid_board([_,_,_]=Rows) ->
+    lists:foldl(fun(Row, Valid) -> Valid andalso is_valid_row(Row) end, true, Rows);
+is_valid_board(_) ->
+    false.
+
+-spec is_valid_row([piece(),...]) -> boolean().
+is_valid_row([_,_,_]=Squares) ->
+    lists:foldl(fun(Square, Valid) -> Valid andalso is_valid_square(Square) end, true, Squares);
+is_valid_row(_) ->
+    false.
+
+-spec is_valid_square(square()) -> boolean().
+is_valid_square(o) -> true;
+is_valid_square(x) -> true;
+is_valid_square(u) -> true;
+is_valid_square(_) -> false.
+
+-spec validate_move(#player{}, board(), reference(), board()) -> 'ok' | {'error', any()}.
+validate_move(Player, OldBoard, Ref, NewBoard) ->
+    #player{ref = PlayerRef, piece = PlayerPiece} = Player,
+    if
+        Ref /= PlayerRef -> {error, invalid_ref};
+        true ->
+            case is_valid_board(NewBoard) of
+                false -> {error, invalid_board};
+                _ ->
+                    case pieces_added(OldBoard, NewBoard) of
+                        [PlayerPiece] -> ok;
+                        _ -> {error, illegal_move}
+                    end
+            end
+    end.
 
 -spec pieces_added(board(), board()) -> [piece()].
 pieces_added(OldBoard, NewBoard) ->
@@ -151,7 +208,7 @@ diag_winner([[P,_,_],[_,P,_],[_,_,P]]) when P /= u -> [P];
 diag_winner([[_,_,P],[_,P,_],[P,_,_]]) when P /= u -> [P];
 diag_winner(_) -> [].
 
--spec notify_player(#player{}, term()) -> ok.
+-spec notify_player(#player{}, any()) -> ok.
 notify_player(#player{pid=Pid}, Msg) ->
     Pid ! Msg,
     ok.
